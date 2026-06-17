7dbfc133444c97119a977705754ca107ee76c273
Msayeem <msayeem223@gmail.com>
2026-06-17 21:10:52 +0600

    Navbar added

---
+++ src/Components/Nav.jsx
@@ -0,0 +1,23 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div className='w-[90%] mx-auto my-2.5'>
+            <nav className='flex justify-between items-center'>
+                <h1>
+                    Startup-Forge
+                </h1>
+
+                <div className='flex items-center gap-3'>
+                    <Link href={'/'}>Home</Link>
+                    <Link href={'/'}>Browse Startups</Link>
+                    <Link href={'/'}>Browse Oppurtunities</Link>
+                    <Link href={'/'}>Login</Link>
+                </div>
+            </nav>
+        </div>
+    );
+};
+
+export default Nav;
